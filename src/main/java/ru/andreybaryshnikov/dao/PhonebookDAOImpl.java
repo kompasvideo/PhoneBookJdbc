14bd054769cd95ba9b3a2df9013afeb62e587b90
Андрей Барышников <andrey1andrey@narod.ru>
2023-08-16 13:50:27 +0300

    3

--- src/main/java/ru/andreybaryshnikov/dao/PhonebookDAOImpl.java
+++ src/main/java/ru/andreybaryshnikov/dao/PhonebookDAOImpl.java
@@ -13,7 +13,7 @@ public class PhonebookDAOImpl implements PhonebookDAO {
     private static final String DRIVER = "org.postgresql.Driver";
     private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
     private static final String USERNAME = "postgres";
-    private static final String PASSWORD = "andrey1978";
+    private static final String PASSWORD = "andrey";
     private static Connection connection;
 
     static {
